c33f4d6f88e2874f0eb9eafee525aa1dcf182809
jw.son <jw.son@lge.com>
2021-07-01 15:03:56 +0900

    Create Android Apk Source

---
+++ app/src/main/java/com/example/upbitautotrade/activity/UpbitAutoTradeMainActivity.java
@@ -0,0 +1,14 @@
+package com.example.upbitautotrade.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpbitAutoTradeMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upbit_auto_trade_main);
+    }
+}
